9ea4fe3b5f0ceeb9ceabdc7fe910384f98ff68e2
eugecm <eugeniocanom@gmail.com>
2017-08-26 12:31:49 +0100

    added initial types

--- metar/metar.go
+++ metar/metar.go
@@ -3,15 +3,48 @@ Package metar includes abstractions to help parse METAR strings.
  */
 package metar
 
-// Field is an abstraction that describes a meteorological feature of a METAR string
-type Field interface {
-	// Name is
-	Name() string
-	HumanString() string
-	String() string
-}
+import "time"
 
-// FieldParser parses a METAR token string and returns a Field (if token is valid)
-type FieldParser interface {
-	GetField(field string) (Field, error)
+const (
+	TypeMetar = iota
+	TypeSpeci
+)
+
+// Type is either METAR os SPECI.
+type Type int8
+// Station is the ICAO location indicator that this report describes.
+type Station string
+// DateTime represents the date and time (UTC) of this report.
+type DateTime time.Time
+// Auto indicates if the report contains only automated observations.
+type Auto bool
+// Wind describes the wind conditions of the report.
+type Wind interface {
+	// Direction (in degrees) where the wind is coming from.
+	Direction() int
+	// Mean speed. Given in knots.
+	Speed() int
+	Gust() int
+	Variation() int
 }
+type Visibility interface {
+	Cavok() bool
+	Distance() int
+	Runway() struct {
+		Threshold int
+	}
+	Weather() string
+	Cloud() string
+}
+type Temperature interface {
+	Temperature() int
+	DewPoint() int
+}
+type Pressure int
+type Supplementary interface {
+	RecentWeather() string
+	WindSheer() struct {
+		Runway int
+	}
+}
+type Remarks string
