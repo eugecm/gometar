caf563a7aaa4d95e5d6cfe2d1fd90089561e81f1
eugecm <eugeniocanom@gmail.com>
2017-08-28 12:18:01 +0100

    Improved specification of Wind and Visibility

--- metar/metar.go
+++ metar/metar.go
@@ -1,33 +1,54 @@
 /*
 Package metar includes abstractions to help parse METAR strings.
- */
+*/
 package metar
 
 import "time"
 
 const (
+	// TypeMetar indicates that the report is of type METAR
 	TypeMetar = iota
+	// TypeSpeci specifies that the report is of type SPECI
 	TypeSpeci
 )
 
 // Type is either METAR os SPECI.
 type Type int8
+
 // Station is the ICAO location indicator that this report describes.
 type Station string
+
 // DateTime represents the date and time (UTC) of this report.
 type DateTime time.Time
+
 // Auto indicates if the report contains only automated observations.
 type Auto bool
+
 // Wind describes the wind conditions of the report.
 type Wind interface {
-	// Direction (in degrees) where the wind is coming from.
-	Direction() int
-	// Mean speed. Given in knots.
+	// Direction returns the direction of the wind in degrees from true north, or an error if
+	// the direction is unknown.
+	Direction() (int, error)
+	// IsVariable determines whether the wind direction varies by more than 60 degrees.
+	IsVariable() bool
+	// Variance returns the starting and ending direction of the wind variance.
+	Variance() (int, int)
+	// Speed is the mean value of the speed during the sampling period (aprox 10 minutes).
 	Speed() int
-	Gust() int
-	Variation() int
+	// Gust returns the maximum wind speed measured during the sampling period. If the maximum
+	// wind speed does not exceed the mean wind speed by more than 10 knots, error will be set
+	// to true indicating that there is no gust.
+	Gust() (int, error)
 }
+
+// Visibility describes the visibility conditions of the report.
 type Visibility interface {
+	// Cavok (Cloud And Visibility OK) indicates that the following conditions are observed
+	// simultaneously:
+	//   - Visibility of 10Km or more.
+	//   - No clouds below 5000ft or highest minimum sector altitude. Whichever is greater.
+	//   - No cumulonimbus or towering cumulus at any level.
+	//   - No significant weather condition.
 	Cavok() bool
 	Distance() int
 	Runway() struct {
