641354fa4a8b6fdbbb797650aa576656f538cf5a
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-17 09:15:30 +0100

    added comment to modifier

--- metar/metar.go
+++ metar/metar.go
@@ -185,7 +185,10 @@ type Report struct {
 		Runway     string
 		Unit       VisibilityUnit
 		Visibility float32
-		Modifier   VisibilityModifier
+		// Modifier is used for expressing bounds within a distance
+		// measurement. Examples: 3000 meters OR MORE, 3/4 Statute miles OR
+		// LESS etc.
+		Modifier VisibilityModifier
 	}
 
 	Weather struct {
