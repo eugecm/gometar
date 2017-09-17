905b2a13fd2e174e93c1a8421e23ece56702187a
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-17 18:28:05 +0100

    documented all fields of the report

--- metar.go
+++ metar.go
@@ -5,6 +5,7 @@ package metar
 
 import "time"
 
+// Report is a meteorological report at an airfield (METAR)
 type Report struct {
 	// Station is the ICAO location indicator that this report describes.
 	Station string
@@ -15,23 +16,33 @@ type Report struct {
 	// Auto indicates if the report contains only automated observations.
 	Auto bool
 
+	// Wind describes wind conditions such as speed and direction.
 	Wind WindGroup
 
 	// Cavok indicates Cloud and Visbility OK. If set to true then Visibility
 	// RunwayVisualRange, Weather and Cloud sections can be ignored.
 	Cavok bool
 
-	// Visibility describes the visibility conditions of the report.
+	// Visibility describes the visibility in the sky at the airfield.
 	Visibility VisibilityGroup
 
 	// RunwayVisualRange gives information about the visibility on the runway
 	RunwayVisualRange RunwayVisualRangeGroup
 
+	// Weather describes the weather conditions at (or near) the airfield.
 	Weather WeatherGroup
 
+	// Clouds describes the clouds in the sky at different altitudes above
+	// the airfield.
 	Clouds []CloudInformation
 
+	// Temperature indicates temperature and dew point information.
 	Temperature TemperatureGroup
-	Qnh         QnhGroup
-	Remarks     RemarksGroup
+
+	// Qnh represents the atmospheric pressure adjusted to sea level.
+	Qnh QnhGroup
+
+	// Remarks contains significant information not covered by the rest of the
+	// METAR.
+	Remarks RemarksGroup
 }
