b7af851daa6343719df24e2403d30add5b94e921
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-24 09:38:13 +0100

    testing wind source

---
+++ wind/parser/parser_test.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestSource(t *testing.T) {
+	var cases = []struct {
+		input    string
+		expected int
+	}{
+		{"19020G26KT", 190},
+		{"13009KT", 130},
+		{"09004KT", 90},
+		{"14002MPS 100V180", 140},
+	}
+
+	parser := New()
+
+	for _, c := range cases {
+		group, err := parser.Parse(c.input)
+		if err != nil {
+			t.Error(err)
+			t.Fail()
+		}
+		if group.Source != c.expected {
+			t.Errorf("expected source to be %v, got %v", c.expected, group.Source)
+			t.Fail()
+		}
+	}
+}
