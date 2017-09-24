f9cd9f2cec97bec7577fb3f910f7274889e62c4a
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-24 17:07:43 +0100

    added wind parsing benchmark

--- wind/parser/parser_test.go
+++ wind/parser/parser_test.go
@@ -55,3 +55,26 @@ func TestWindSource(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkWindParsing(b *testing.B) {
+	cases := []string{
+		"29008KT",
+		"19011KT",
+		"32007KT",
+		"09007KT",
+		"27006KT",
+		"VRB04KT",
+		"36001KT",
+		"07004KT",
+		"10003MPS",
+		"35004KT",
+		"28010KT",
+		"00000KT",
+	}
+	casesN := len(cases)
+	parser := New()
+
+	for i := 0; i < b.N; i++ {
+		parser.Parse(cases[i%casesN])
+	}
+}
