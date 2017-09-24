0c1a1f1ea911b2331abedf9df8cda102dbfed205
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-24 16:00:33 +0100

    added more documentation to the wind parser

--- wind/parser/parser.go
+++ wind/parser/parser.go
@@ -2,13 +2,18 @@ package parser
 
 import "github.com/eugecm/gometar/wind"
 
+// WParser is an implementation of WindParser
 type WParser struct {
 }
 
+// New creates an instance of WParser
 func New() wind.WindParser {
 	return &WParser{}
 }
 
+// Parse takes a string representing the Wind component of a METAR report
+// and builds a corresponding wind.Group (or an error if the wind information
+// could not be parsed
 func (w *WParser) Parse(input string) (wind.Group, error) {
 	return wind.Group{}, nil
 }
