67f2d92d99013867f2fb478a4391257fb0e3881e
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2017-09-24 16:49:23 +0100

    implemented wind variable and wind source

--- wind/parser/parser.go
+++ wind/parser/parser.go
@@ -1,19 +1,60 @@
 package parser
 
-import "github.com/eugecm/gometar/wind"
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/eugecm/gometar/wind"
+)
+
+var groupRegexps = []string{
+	`(?P<source>VRB|[0-9]{3})`,
+	`(?P<gust>[0-9]{2}G)?`,
+	`(?P<speed>[0-9]{2})`,
+	`(?P<unit>KT|MPS)`,
+	`( `,
+	`(?P<varifrom>[0-9]{3})V(?P<varito>[0-9]{3})`,
+	`)?`,
+}
 
 // WParser is an implementation of WindParser
 type WParser struct {
+	groupRegexp *regexp.Regexp
 }
 
 // New creates an instance of WParser
 func New() wind.WindParser {
-	return &WParser{}
+	groupRegexpString := strings.Join(groupRegexps, "")
+	groupRegexp := regexp.MustCompile(groupRegexpString)
+
+	return &WParser{groupRegexp: groupRegexp}
 }
 
 // Parse takes a string representing the Wind component of a METAR report
 // and builds a corresponding wind.Group (or an error if the wind information
 // could not be parsed
 func (w *WParser) Parse(input string) (wind.Group, error) {
-	return wind.Group{}, nil
+	matches := w.groupRegexp.FindStringSubmatch(input)
+
+	// get variable component
+	variable := false
+	if matches[1] == "VRB" {
+		variable = true
+	}
+
+	// get source component
+	source := 0
+	if !variable { // source is only given in non-variable reports
+		sourceInt, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return wind.Group{}, err
+		}
+		source = sourceInt
+	}
+
+	return wind.Group{
+		Variable: variable,
+		Source:   source,
+	}, nil
 }
