0552195cac8a932368dcbd6de539e22f2752d702
Eugenio Cano-Manuel Mendoza <eugeniocanom@gmail.com>
2017-10-21 21:06:49 +0100

    improved cloud information spec

--- sky/sky.go
+++ sky/sky.go
@@ -1,28 +1,32 @@
 package sky
 
-type CloudAmount int
+type CloudAmount string
 
 const (
-	CloudAmountFew CloudAmount = iota
-	CloudAmountScattered
-	CloudAmountBroken
-	CloudAmountOvercast
-	CloudAmountNilSignificant
-	CloudAmountNilDetected
+	CloudAmountFew            CloudAmount = "FEW"
+	CloudAmountScattered      CloudAmount = "SCT"
+	CloudAmountBroken         CloudAmount = "BKN"
+	CloudAmountOvercast       CloudAmount = "OVC"
+	CloudAmountNilSignificant CloudAmount = "NSC"
+	CloudAmountSkyClear       CloudAmount = "SKC"
+	CloudAmountClear          CloudAmount = "CLR"
+	CloudAmountCannotBeSeen   CloudAmount = "VV"
 )
 
-type CloudType int
+type CloudType string
 
 const (
-	CloudTypeNone CloudType = iota
-	CloudTypeToweringCumulus
-	CloudTypeCumulonimbusOrashowerThunderstorm
-	CloudTypeAltocumulusCastellanus
+	CloudTypeNone            CloudType = ""
+	CloudTypeToweringCumulus CloudType = "TCU"
+	CloudTypeCumulonimbus    CloudType = "CB"
 )
 
 type CloudInformation struct {
+	Height string
 	Amount CloudAmount
-	// Height is how high the cloud is from the airfield (in feet)
-	Height int
 	Type   CloudType
+}
+
+type Parser interface {
+	Parse(input string) ([]CloudInformation, error)
 }
